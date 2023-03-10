7345306ff55b8d0f160bb29702cc1badc5b99be7
Deepak <dgusaiwal537@Gmail.com>
2023-03-10 23:02:04 +0530

    use height property to get exact value of top val for sidebar

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ let sidebar = document.querySelector('.sidebar');
 let sidebarUl = document.querySelector('.sidebar ul')
 let lis = [...sidebarUl.children];
 let rect = sidebarUl.getBoundingClientRect();
-lis.forEach(li => {
+lis.forEach((li,index) => {
     let dimentions =li.getBoundingClientRect();
     li.addEventListener('click',()=>{
-        setSidebarTopVal(dimentions.top);
+        console.log('element dimentions',dimentions);
+        let gaps = 16; // it is the amount of gap between li's witch is 16px 1rem;
+        let calculateExactDimention = (dimentions.height * index) + (gaps * index);
+        setSidebarTopVal(calculateExactDimention);
     })
 });
 
@@ -14,4 +17,5 @@ const setSidebarTopVal = (val)=>{
     topVal = val ;
 sidebar.style.top = `${-topVal}px`;
 sidebar.style.background = `red`;
+console.log('sidebar dimensions',sidebar.getBoundingClientRect())
 }
